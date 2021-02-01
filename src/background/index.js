514f0df280b8e750d54f20120797fd4caa196858
syJSdev <whitedragon226@gmail.com>
2021-02-01 05:17:00 -0500

    background.attach parameter root is required now.

--- src/background/index.js
+++ src/background/index.js
@@ -12,7 +12,7 @@ export default class Step {
     this.active = false;
     this.context = context;
   }
-  attach(root = "body") {
+  attach(root) {
     u(root).append(this.el);
   }
   remove() {
