efcf0d8300a19b4d944dfc5c59c1f3e5c2c8bbba
Eugene Trounev <eugene.trounev@gmail.com>
2024-09-08 21:46:46 -0400

    Rebuilt the library

--- tourguide.js
+++ tourguide.js
@@ -1835,10 +1835,141 @@ var Tourguide = (function () {
     }
   }
 
-  var e={"":["<em>","</em>"],_:["<strong>","</strong>"],"*":["<strong>","</strong>"],"~":["<s>","</s>"],"\n":["<br />"]," ":["<br />"],"-":["<hr />"]};function n(e){return e.replace(RegExp("^"+(e.match(/^(\t| )+/)||"")[0],"gm"),"")}function r(e){return (e+"").replace(/"/g,"&quot;").replace(/</g,"&lt;").replace(/>/g,"&gt;")}function t(a,c){var o,l,g,s,p,u=/((?:^|\n+)(?:\n---+|\* \*(?: \*)+)\n)|(?:^``` *(\w*)\n([\s\S]*?)\n```$)|((?:(?:^|\n+)(?:\t|  {2,}).+)+\n*)|((?:(?:^|\n)([>*+-]|\d+\.)\s+.*)+)|(?:!\[([^\]]*?)\]\(([^)]+?)\))|(\[)|(\](?:\(([^)]+?)\))?)|(?:(?:^|\n+)([^\s].*)\n(-{3,}|={3,})(?:\n+|$))|(?:(?:^|\n+)(#{1,6})\s*(.+)(?:\n+|$))|(?:`([^`].*?)`)|(  \n\n*|\n{2,}|__|\*\*|[_*]|~~)/gm,m=[],h="",i=c||{},d=0;function f(n){var r=e[n[1]||""],t=m[m.length-1]==n;return r?r[1]?(t?m.pop():m.push(n),r[0|t]):r[0]:n}function $(){for(var e="";m.length;)e+=f(m[m.length-1]);return e}for(a=a.replace(/^\[(.+?)\]:\s*(.+)$/gm,function(e,n,r){return i[n.toLowerCase()]=r,""}).replace(/^\n+|\n+$/g,"");g=u.exec(a);)l=a.substring(d,g.index),d=u.lastIndex,o=g[0],l.match(/[^\\](\\\\)*\\$/)||((p=g[3]||g[4])?o='<pre class="code '+(g[4]?"poetry":g[2].toLowerCase())+'"><code'+(g[2]?' class="language-'+g[2].toLowerCase()+'"':"")+">"+n(r(p).replace(/^\n+|\n+$/g,""))+"</code></pre>":(p=g[6])?(p.match(/\./)&&(g[5]=g[5].replace(/^\d+/gm,"")),s=t(n(g[5].replace(/^\s*[>*+.-]/gm,""))),">"==p?p="blockquote":(p=p.match(/\./)?"ol":"ul",s=s.replace(/^(.*)(\n|$)/gm,"<li>$1</li>")),o="<"+p+">"+s+"</"+p+">"):g[8]?o='<img src="'+r(g[8])+'" alt="'+r(g[7])+'">':g[10]?(h=h.replace("<a>",'<a href="'+r(g[11]||i[l.toLowerCase()])+'">'),o=$()+"</a>"):g[9]?o="<a>":g[12]||g[14]?o="<"+(p="h"+(g[14]?g[14].length:g[13]>"="?1:2))+">"+t(g[12]||g[15],i)+"</"+p+">":g[16]?o="<code>"+r(g[16])+"</code>":(g[17]||g[1])&&(o=f(g[17]||"--"))),h+=l,h+=o;return (h+a.substring(d)+$()).replace(/^\n+|\n+$/g,"")}
+  /**
+  The MIT License (MIT)
+
+  Copyright (c) 2017 Jason Miller
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy of
+  this software and associated documentation files (the "Software"), to deal in
+  the Software without restriction, including without limitation the rights to
+  use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+  the Software, and to permit persons to whom the Software is furnished to do so,
+  subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+  FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+  COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+  IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+  https://github.com/developit/snarkdown
+  */
+
+  const TAGS = {
+    '': ['<em>', '</em>'],
+    '_': ['<strong>', '</strong>'],
+    '*': ['<strong>', '</strong>'],
+    '~': ['<s>', '</s>'],
+    '\n': ['<br />'],
+    ' ': ['<br />'],
+    '-': ['<hr />']
+  };
+
+  /**
+   * Outdent a string based on the first indented line's leading whitespace
+   * @private
+   */
+  function outdent(str) {
+    return str.replace(RegExp('^' + (str.match(/^(\t| )+/) || '')[0], 'gm'), '');
+  }
+
+  /**
+   * Encode special attribute characters to HTML entities in a String.
+   * @private
+   */
+  function encodeAttr(str) {
+    return (str + '').replace(/"/g, '&quot;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+  }
+
+  /**
+   * Parse Markdown into an HTML String.
+   */
+  function parse(md, prevLinks) {
+    let tokenizer = /((?:^|\n+)(?:\n---+|\* \*(?: \*)+)\n)|(?:^``` *(\w*)\n([\s\S]*?)\n```$)|((?:(?:^|\n+)(?:\t|  {2,}).+)+\n*)|((?:(?:^|\n)([>*+-]|\d+\.)\s+.*)+)|(?:!\[([^\]]*?)\]\(([^)]+?)\))|(\[)|(\](?:\(([^)]+?)\))?)|(?:(?:^|\n+)([^\s].*)\n(-{3,}|={3,})(?:\n+|$))|(?:(?:^|\n+)(#{1,6})\s*(.+)(?:\n+|$))|(?:`([^`].*?)`)|(  \n\n*|\n{2,}|__|\*\*|[_*]|~~)/gm;
+    let context = [];
+    let out = '';
+    let links = prevLinks || {};
+    let last = 0;
+    let chunk = null;
+    let prev = '';
+    let token = null;
+    let inner = '';
+    let t = '';
+    function tag(token) {
+      const desc = TAGS[token[1] || ''];
+      const end = context[context.length - 1] === token;
+      if (!desc) return token;
+      if (!desc[1]) return desc[0];
+      if (end) context.pop();else context.push(token);
+      return desc[end | 0];
+    }
+    function flush() {
+      let str = '';
+      while (context.length > 0) str += tag(context[context.length - 1]);
+      return str;
+    }
+    md = md.replace(/^\[(.+?)\]:\s*(.+)$/gm, (_s, name, url) => {
+      links[name.toLowerCase()] = url;
+      return '';
+    }).replace(/^\n+|\n+$/g, '');
+    while (token = tokenizer.exec(md)) {
+      prev = md.substring(last, token.index);
+      last = tokenizer.lastIndex;
+      chunk = token[0];
+      if (prev.match(/[^\\](\\\\)*\\$/)) ;
+      // Code/Indent blocks:
+      else if (t = token[3] || token[4]) {
+        chunk = '<pre class="code ' + (token[4] ? 'poetry' : token[2].toLowerCase()) + '"><code' + (token[2] ? " class=\"language-".concat(token[2].toLowerCase(), "\"") : '') + '>' + outdent(encodeAttr(t).replace(/^\n+|\n+$/g, '')) + '</code></pre>';
+      }
+      // > Quotes, -* lists:
+      else if (t = token[6]) {
+        if (t.match(/\./)) {
+          token[5] = token[5].replace(/^\d+/gm, '');
+        }
+        inner = parse(outdent(token[5].replace(/^\s*[>*+.-]/gm, '')));
+        if (t == '>') t = 'blockquote';else {
+          t = t.match(/\./) ? 'ol' : 'ul';
+          inner = inner.replace(/^(.*)(\n|$)/gm, '<li>$1</li>');
+        }
+        chunk = '<' + t + '>' + inner + '</' + t + '>';
+      }
+      // Images:
+      else if (token[8]) {
+        chunk = "<img src=\"".concat(encodeAttr(token[8]), "\" alt=\"").concat(encodeAttr(token[7]), "\">");
+      }
+      // Links:
+      else if (token[10]) {
+        out = out.replace('<a>', "<a href=\"".concat(encodeAttr(token[11] || links[prev.toLowerCase()]), "\" target=\"_blank\">"));
+        chunk = flush() + '</a>';
+      } else if (token[9]) {
+        chunk = '<a>';
+      }
+      // Headings:
+      else if (token[12] || token[14]) {
+        t = 'h' + (token[14] ? token[14].length : token[13] > '=' ? 1 : 2);
+        chunk = '<' + t + '>' + parse(token[12] || token[15], links) + '</' + t + '>';
+      }
+      // `code`:
+      else if (token[16]) {
+        chunk = '<code>' + encodeAttr(token[16]) + '</code>';
+      }
+      // Inline formatting: *em*, **strong** & friends
+      else if (token[17] || token[1]) {
+        chunk = tag(token[17] || '--');
+      }
+      out += prev;
+      out += chunk;
+    }
+    return (out + md.substring(last) + flush()).replace(/^\n+|\n+$/g, '');
+  }
 
   const MarkdownDecorator = new ContentDecorator("", text => {
-    return t(text);
+    return parse(text);
   });
 
   class CardStep extends PopoverStep {
